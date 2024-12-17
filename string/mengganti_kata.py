514d4bd9a5bc3284b2e1bf729369222209e4bc98
= <=>
2024-12-17 10:50:48 +0700

    string

---
+++ string/mengganti_kata.py
@@ -0,0 +1,7 @@
+teks = "hello, world!"
+teks = teks.strip()
+print("setelah strip :",teks)
+print("huruf besar :", teks.upper())
+print("huruf kecil :", teks.lower())
+print("mengganti 'hello' dengan 'hi' :", teks.replace("hello", "hi"))
+
